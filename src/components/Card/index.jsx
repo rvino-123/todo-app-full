3774893a4c041745f2774e089e05dd44b09e34b6
Roman Vinokurov <roman.vinokurov96@gmail.com>
2022-04-22 13:15:30 +0100

    Revert "added a backend server and some code cleanup."
    This reverts commit 85ebbc7a96ed3c51ad64c0de80a397810ed549e9.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,7 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Card = styled.div`
   border-radius: 16px;
-`
+`;
 
-export default Card
+export default Card;
